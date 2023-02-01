1eb0fc6aae1029f6c6eccef49ae815bec4ecb5d9
Basic_Beauty <Piera-nyaga>
2023-02-01 22:11:07 +0300

    second commit

--- dist/assess.js
+++ dist/assess.js
@@ -26,8 +26,6 @@ let incomedisp = document.querySelector('#incomeTr');
 let expensedisp = document.querySelector('#expenseTr');
 let balanceTracker = document.querySelector('.balance');
 let incomeType = document.getElementById('incometype');
-// let takeIncome = document.getElementById('income')! as HTMLOptionElement;
-// let takeExpense = document.getElementById('expense')!as HTMLOptionElement;
 let Amount = document.getElementById('amount');
 let submit = document.getElementById('sub');
 class Income {
@@ -46,18 +44,8 @@ class Expense {
 // }
 class Details {
     constructor() {
-        // date: string;
-        // name: string;
-        // amountInc: number;
         this.balance = 0;
     }
-    // expenses: Expense[] = [];
-    // constructor(date: string, amountInc: number, name: string) {
-    //     this.date = date;
-    //     this.name= name;
-    //     this.amountInc= amountInc;
-    // }
-    // get the balance income- expenses
     getBalance(amountInc, amountExp) {
         this.balance = amountInc - amountExp;
         return this.balance;
@@ -65,73 +53,43 @@ class Details {
 }
 const Consumed = [];
 const Earned = [];
-function showExpense() {
-    if (incomeType.value == 'expense') {
-        const totalExpense = Amount.value;
-        let theExpense = totalExpense;
-        Consumed.push(theExpense);
-        let numConsumed = Consumed.map(Number);
-        const consumed = numConsumed.reduce((a, b) => {
-            return a + b;
-        }, 0);
-        console.log(consumed);
-        console.log(expensedisp);
-        expensedisp.innerHTML = '';
-        let html = `
-    <h2> EXPENSES </h2>
-     <p> $ ${consumed} </p>
-    
-    `;
-        expensedisp.innerHTML += html;
-    }
-    else {
-        console.log("HELLO");
-    }
-}
 submit.addEventListener('click', e => {
     e.preventDefault();
     showExpense();
+    function showExpense() {
+        if (incomeType.value == 'expense') {
+            const totalExpense = Amount.value;
+            let theExpense = totalExpense;
+            Consumed.push(theExpense);
+            let numConsumed = Consumed.map(Number);
+            const consumed = numConsumed.reduce((a, b) => {
+                return a + b;
+            }, 0);
+            // console.log(consumed)
+            // console.log(expensedisp)
+            expensedisp.innerHTML = '';
+            let html = `
+    <h2> Expenses </h2>
+     <p> $ ${consumed} </p>
+    
+    `;
+            expensedisp.innerHTML += html;
+        }
+        else {
+            const totalIncome = Amount.value;
+            let theIncome = totalIncome;
+            Earned.push(theIncome);
+            let numEarned = Earned.map(Number);
+            const earned = numEarned.reduce((a, b) => {
+                return a + b;
+            }, 0);
+            incomedisp.innerHTML = '';
+            let html = `
+    <h2> Income </h2>
+     <p> $ ${earned} </p>
+    
+    `;
+            incomedisp.innerHTML += html;
+        }
+    }
 });
-// const showExpense = (expense: Expense) => {
-//     let theexpense: string = `
-//     <h3> ${expense.name}</h3>
-//     <div class="expense-value">
-//         <p>\$ ${expense.amountExp}</p>
-//     </div>
-//     <div class="date">
-//         <p>${expense.date}</p>
-//     </div>`
-//     return theexpense
-// }
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     let amountInc = form.income.value;
-//     let expenseittem = form.category.value;
-//     let amount = form.amount.value;
-//     let date = form.date.value;
-//     console.log(date, amountInc, amount, expenseittem);
-//     let dets = new Details(date, amount, amountInc);
-//     let expense = new Expense(date, amount, expenseittem);
-//     dets.expenses.push(expense);
-//     balanceTracker.innerHTML = `
-//     <h1>Income</h1>
-//         <div class="income-item">
-//                 <h3>Salary</h3>
-//                 <p>\$ ${dets.amountInc}</p>
-//     </div>`
-//     let tracker: string = ` <h1>expence tracker</h1>
-//     <h2>Balance </h2>
-//         <p>${dets.getBalance()}</p>
-//         <br>
-//         ${dets.expenses.map(expense => showExpense(expense)).join('')}
-//     `
-//     balanceTracker.insertAdjacentHTML('afterbegin', tracker);
-//     dets.expenses.forEach(expense => {
-//     expensedisp.innerHTML = `
-//     <h1>expense</h1>
-//     <div class="expense-item">
-//         <h3>${expense.name}</h3>
-//         <p>$ ${expense.amountExp}</p>
-//     </div>`
-//     })
-// })
--- src/assess.ts
+++ src/assess.ts
@@ -39,17 +39,15 @@ let form = document.querySelector('.form')! as HTMLFormElement;
 let incomedisp = document.querySelector('#incomeTr')!;
 let expensedisp = document.querySelector('#expenseTr')!;
 let balanceTracker = document.querySelector('.balance')!;
-let incomeType=document.getElementById('incometype')! as HTMLInputElement;
-// let takeIncome = document.getElementById('income')! as HTMLOptionElement;
-// let takeExpense = document.getElementById('expense')!as HTMLOptionElement;
-let Amount= document.getElementById('amount')! as HTMLFormElement;
-let submit= document.getElementById('sub')! as HTMLButtonElement
-//interface for all records
+let incomeType = document.getElementById('incometype')! as HTMLInputElement;
+let Amount = document.getElementById('amount')! as HTMLFormElement;
+let submit = document.getElementById('sub')! as HTMLButtonElement
 
-// interface records {
-//     income?: [];
-//     expense?: [];
-// }
+//interface for all records
+interface records {
+    income?: number;
+    expense?: number;
+}
 
 //class and interface for income
 
@@ -57,10 +55,10 @@ interface income {
     amountInc: number;
 }
 
-class Income implements income{
+class Income implements income {
     amountInc: number;
     constructor(date: string, amountInc: number) {
-        this.amountInc= amountInc;
+        this.amountInc = amountInc;
 
     }
 
@@ -68,10 +66,10 @@ class Income implements income{
 
 // class and interface for expenses
 
-interface expense{
+interface expense {
     amountExp: number;
 }
-    
+
 
 class Expense implements expense {
     amountExp: number;
@@ -80,7 +78,7 @@ class Expense implements expense {
 
     }
 
-} 
+}
 
 //class and interface for the balances
 
@@ -89,116 +87,75 @@ class Expense implements expense {
 // }
 
 class Details {
-    // date: string;
-    // name: string;
-    // amountInc: number;
-    private balance: number=0;
-    // expenses: Expense[] = [];
+    private balance: number = 0;
 
-    // constructor(date: string, amountInc: number, name: string) {
-    //     this.date = date;
-    //     this.name= name;
-    //     this.amountInc= amountInc;
-
-    // }
-    // get the balance income- expenses
-    
-    getBalance(amountInc:number, amountExp:number) {
+    getBalance(amountInc: number, amountExp: number) {
         this.balance = amountInc - amountExp
         return this.balance;
     }
 
 }
 
-const Consumed : Expense[]=[];
+const Consumed: Expense[] = [];
 
-const Earned: Income[]=[];
+const Earned: Income[] = [];
 
-function showExpense(){
-    
+submit.addEventListener('click', e => {
+    e.preventDefault()
+    showExpense()
 
-    if (incomeType.value=='expense') {
-        const totalExpense= Amount.value;
-    let theExpense: Expense= totalExpense
-    Consumed.push(theExpense);
-    let numConsumed = Consumed.map(Number)
+    function showExpense() {
 
-    const consumed= numConsumed.reduce((a, b) =>{ 
-        return a+b   },0 )
 
-    console.log(consumed)
-    console.log(expensedisp)
-    
-    expensedisp.innerHTML='';
+        if (incomeType.value == 'expense') {
+            const totalExpense = Amount.value;
+            let theExpense: Expense = totalExpense
+            Consumed.push(theExpense);
+            let numConsumed = Consumed.map(Number)
 
-    let html=`
-    <h2> EXPENSES </h2>
+            const consumed = numConsumed.reduce((a, b) => {
+                return a + b
+            }, 0)
+
+            // console.log(consumed)
+            // console.log(expensedisp)
+
+            expensedisp.innerHTML = '';
+
+            let html = `
+    <h2> Expenses </h2>
      <p> $ ${consumed} </p>
     
     `
-    expensedisp.innerHTML += html;
+            expensedisp.innerHTML += html;
 
-        
-    } else{
-        console.log("HELLO")
+
+        } else {
+            const totalIncome = Amount.value;
+            let theIncome: Income = totalIncome
+            Earned.push(theIncome);
+            let numEarned = Earned.map(Number)
+
+            const earned = numEarned.reduce((a, b) => {
+                return a + b
+            }, 0)
+
+
+            incomedisp.innerHTML = '';
+
+            let html = `
+    <h2> Income </h2>
+     <p> $ ${earned} </p>
+    
+    `
+            incomedisp.innerHTML += html;
+
+        }
+
     }
-    
-}
 
+})
 
-submit.addEventListener('click', e =>{
-    e.preventDefault()
-    showExpense()
-})                                                        
-// const showExpense = (expense: Expense) => {
-//     let theexpense: string = `
-//     <h3> ${expense.name}</h3>
-//     <div class="expense-value">
-//         <p>\$ ${expense.amountExp}</p>
-//     </div>
-//     <div class="date">
-//         <p>${expense.date}</p>
-//     </div>`
-//     return theexpense
-// }
-
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     let amountInc = form.income.value;
-//     let expenseittem = form.category.value;
-//     let amount = form.amount.value;
-//     let date = form.date.value;
-    
-//     console.log(date, amountInc, amount, expenseittem);
-//     let dets = new Details(date, amount, amountInc);
-//     let expense = new Expense(date, amount, expenseittem);
-//     dets.expenses.push(expense);
-
-//     balanceTracker.innerHTML = `
-//     <h1>Income</h1>
-//         <div class="income-item">
-//                 <h3>Salary</h3>
-//                 <p>\$ ${dets.amountInc}</p>
-//     </div>`
-//     let tracker: string = ` <h1>expence tracker</h1>
-//     <h2>Balance </h2>
-//         <p>${dets.getBalance()}</p>
-//         <br>
-//         ${dets.expenses.map(expense => showExpense(expense)).join('')}
-//     `
-
-
-//     balanceTracker.insertAdjacentHTML('afterbegin', tracker);
-//     dets.expenses.forEach(expense => {
-//     expensedisp.innerHTML = `
-//     <h1>expense</h1>
-//     <div class="expense-item">
-//         <h3>${expense.name}</h3>
-//         <p>$ ${expense.amountExp}</p>
-//     </div>`
-//     })
-
-// })
 
 
 
